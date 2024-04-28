5075e895dd29c55f7fcebb3d2891984ce7366d48
Somasundararam <somus392@gmail.com>
2024-04-28 12:33:47 +0530

    Rarabolcks

---
+++ src/body.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import '../src/Source/body.css'
+export default function body() {
+  return (
+    <div className='main'>
+      <div className='para'>
+    <h2>About Rareblocks</h2>
+    <p className='para'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam dictum aliquet accumsan porta lectus ridiculus in mattis. Netus sodales in volutpat ullamcorper amet adipiscing fermentum.</p>
+        </div>
+        <div className='main-wrapper'>
+    <div className='wrapper'>
+    <h3>Company</h3>
+    <p>About </p>
+    <p>Features</p>
+    <p>Works</p>
+    <p>Career</p>
+    </div>
+    <div className='wrapper'>
+    <h3>Help</h3>
+    <p>Customer Support</p>
+    <p>Delivery Details</p>
+    <p>Terms & Conditions</p>
+    <p>Privacy Policy</p>
+    </div>
+    <div className='wrapper'>
+    <h3>Resources</h3>
+          <p>Free eBooks</p>
+          <p>Development Tutorial</p>
+          <p>How to - Blog</p>
+          <p>Youtube Playlist</p>
+          </div>
+          </div>
+          </div>
+                    )
+          }
+          
